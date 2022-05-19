4d1d121cbe60c2630bb5ec249abbd8647faa95ce
Metehan Gultekin <mgmetehan@hotmail.com>
2022-05-19 22:12:11 +0300

    Add views
    Add views index, kayitEkle, kayitListele and layout

--- Otopark-Uygulmasi/Controllers/HomeController.cs
+++ Otopark-Uygulmasi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Otopark_Uygulmasi.Models.db;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,21 @@ namespace Otopark_Uygulmasi.Controllers
 {
     public class HomeController : Controller
     {
+        db_otoparkEntities db = new db_otoparkEntities();
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult kayitekle()
+        {
+            return View();
+        }
+        public ActionResult kayitlistele()
+        {
+            var liste = db.kisiler.ToList();
+            return View(liste);
+        }
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
