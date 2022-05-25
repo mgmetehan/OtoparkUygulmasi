b239c836f67e7bd3e0fbf3748df030e97c1e3a21
Metehan Gultekin <mgmetehan@hotmail.com>
2022-05-25 22:15:51 +0300

    Update DbQuery

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -36,4 +36,30 @@ kullaniciId int IDENTITY(1,1) Not Null Primary Key,
 kullaniciAd nVarchar(20) Not Null,
 kullaniciSifre nVarchar(15) Not Null
 );
+
+Insert Into giris(kullaniciAd,kullaniciSifre) Values('admin','1234');
+Insert Into giris(kullaniciAd,kullaniciSifre) Values('Metehan','toor');
+
+Insert Into kisiler(kisiTc,kisiAd,kisiSoyad,kisiTelNo,Adres) Values ('11111111111','Metehan','Gultekin','0531','Daire 8');
+Insert Into kisiler(kisiTc,kisiAd,kisiSoyad,kisiTelNo,Adres) Values ('22222222222','Ýlayda','Tan','0540','Daire 3');
+Insert Into kisiler(kisiTc,kisiAd,kisiSoyad,kisiTelNo,Adres) Values ('33333333333','Hilal','Ayþe','0565','Daire 2');
+
+Insert Into otoparklar(otoparkAd,otoparkSehir) Values ('Kaplan','Mersin');
+Insert Into otoparklar(otoparkAd,otoparkSehir) Values ('Yavuz','Ankara');
+Insert Into otoparklar(otoparkAd,otoparkSehir) Values ('Kýlýc','Istanbul');
+
+Insert Into araclar(aracPlaka,aracMarka,aracRenk,kisiId) Values ('34EF60','BMW','Mavi',1);
+Insert Into araclar(aracPlaka,aracMarka,aracRenk,kisiId) Values ('06TY24','Audi','Mavi',2);
+Insert Into araclar(aracPlaka,aracMarka,aracRenk,kisiId) Values ('70ZX08','BMW','Siyah',3);
+
+Insert Into parkYeri(parkYeri,aracId) Values ('1',1);
+Insert Into parkYeri(parkYeri,aracId) Values ('2',3);
+Insert Into parkYeri(parkYeri,aracId) Values ('3',2);
 */
+
+
+
+
+
+
+
